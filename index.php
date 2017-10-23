de2ac2b213fab34921ca7ab11230975c19b3bebb
Bruno <brlaranjeira@gmail.com>
2017-10-23 17:05:12 -0200

    ajusta index para redirecionar corretamente

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?
 require_once (__DIR__ . '/dao/Usuario.php');
+$diretorio = dirname($_SERVER['PHP_SELF']) . '/';
+
+
 $usr = Usuario::restoreFromSession();
 $to = isset($usr) ? './main.php' : './login.php';
-header('Location: ' . $to );
+header("Location: $diretorio$to" );
