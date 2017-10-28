e7ba69e8d4928a056cccd12993fd2edcfebe51d6
Bruno <brlaranjeira@gmail.com>
2017-10-28 13:05:21 -0200

    cria tela de deferir

---
+++ deferir.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brlaranjeira
+ * Date: 10/28/17
+ * Time: 1:03 PM
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Ponto Bolsistas CTISM">
+    <meta name="author" content="Noronha">
+    <link rel="icon" href="img/CTISM.ico">
+    <title>Ponto Bolsistas</title>
+</head>
+<body>
+
+<?
+include "./fragment/header.php";
+
+?>
+
+DEFERIR
