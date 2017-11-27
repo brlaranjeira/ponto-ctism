da527ce0941b669823662a41d5df2e28f1437761
Bruno <brlaranjeira@gmail.com>
2017-11-27 17:37:02 -0200

    faz acao de justificar via ajax, retornando mensagem de sucesso/falha; TODO: fazer mandar email para orientador

--- js/justificar.js
+++ js/justificar.js
@@ -6,3 +6,21 @@ if ($spanMensagem.length) {
     var mensagem = $spanMensagem.text();
     showMessage('[PONTO ELETRÔNICO]',mensagem);
 }
+
+$('#form-justificar').submit( function ( evt ) {
+    $.ajax('./ajax/actionjustificar.php', {
+        method: 'post',
+        data: {
+            registroevt: document.getElementById('registro-evt').value,
+            registromotivo: document.getElementById('registro-motivo').value,
+            registrodthr: document.getElementById('registro-dthr').value
+        }, success: function ( response ) {
+            response = JSON.parse(response)
+            showModal( '<h7>'+response.message+'</h7>' );
+            //TODO: Adicionar campo de email
+        }, error: function ( response ) {
+            showMessage('[PONTO ELETRÔNICO]', response.responseText,'danger');
+        }
+    });
+    return false;
+} );
